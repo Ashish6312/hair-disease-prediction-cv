a1f984ced320fea8d0b1868b24ef1060dffe2a1f
ashish6312 <ashishsharma120512@gmail.com>
2025-11-28 15:33:13 +0530

    Update build script for Render deployment

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
-pip install -r requirements_production.txt
-
 # Navigate to Django project directory
 cd minor
+
+# Install dependencies
+pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
